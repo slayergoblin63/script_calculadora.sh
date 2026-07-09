4e6a85d3a6905997b69db14d42dce4e7358f8731
slayergoblin63 <slayergoblin63@gmail.com>
2026-07-09 19:00:51 -0300

    Update script_calculadora.sh

--- script_calculadora.sh
+++ script_calculadora.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#Esses comandos iram atualizar os pacotes instalados e em seguida instalara o python, caso não esteja instalado! 
 sudo apt update
 sudo apt install python3
 
-python3 calculadora.py
+# Já esse ira execultar o escript da calculadora.py, que por vez mostra todos os caminhos até onde está o código em python da nossa calculadora!
+python3 /home/usuario/thiago/modulo/linux/calculadora.py
