23af97c74da25aaf1933db4583d6928d2dd7c4c8
slayergoblin63 <slayergoblin63@gmail.com>
2026-07-09 19:53:51 -0300

    Add files via upload

---
+++ tutorial_calculadora.sh
@@ -0,0 +1,26 @@
+#PASSOS A SEGUIR PARA EXECULÇÃO DO ARQUIVO_SH!
+
+
+-1 > Possuir o árquivo que deseja execultar baixado na maquina local!.
+
+
+-2 > Já possuindo o árquivo baixado, ira iniciar o seu temial linux para execulção direta do árquivo.
+
+ -- Ultilizando os comandos (PWD) ira conseguir ver o caminho é a raiz de onde se é armazenado todos os diretórios.
+
+ -- O comando (CD diretório que deseja) adentra diretamente na raiz do diretório permetindo a manipulação ou gerenciamento mais precisso dos árquivos.
+
+ -- Encontrando o árquivo.sh, confime sé ele está execúltavel. 
+
+
+-3 > Caso não possua as devidas permissões para a execulção ira seguir os seguintes passos.
+
+ -- Ira buscas onde se encontra o árquivo que desejá execultar no diretório atual.
+
+ -- Caso o arquivo enteja de forma que não seja possivel execultalo, então tera que deixalo de forma que seja possível.
+ 
+ -- Execulte o seguinte código (LL) para ver as permisões atuais do árquivo, assim que já esteja vendo as permissões
+ execulte outro camando para inserir as permissões de execulção (chmod 744), esse comando irá tornalo execúltavel para o usuário
+ é somente leitura para grupos é outros. 
+
+ -- Após tornalo execultave ira por fim, execultar um último comando para inícialo (./nome_do_arquivo.sh).
